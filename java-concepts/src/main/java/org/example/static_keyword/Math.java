70b645a3b5a4f026e8279c4f45bdc00f86a4b249
Shashank Chauhan <shashank.chauhan@comviva.com>
2024-06-19 18:03:48 +0530

    Static keyword

---
+++ java-concepts/src/main/java/org/example/static_keyword/Math.java
@@ -0,0 +1,9 @@
+package org.example.static_keyword;
+
+public class Math {
+    public static final double PI=3.14;
+
+    public static double getCircleArea(int radius) {
+        return PI*radius*radius;
+    }
+}
