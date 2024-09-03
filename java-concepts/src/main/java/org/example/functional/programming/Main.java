f9bcab6be2f947f9dfddca2b7fe51ed6a2a427e8
sscstack <shashanktech2@gmail.com>
2024-09-03 20:48:06 +0530

    Functional Programming

--- java-concepts/src/main/java/org/example/functional/programming/Main.java
+++ java-concepts/src/main/java/org/example/functional/programming/Main.java
@@ -73,6 +73,6 @@ public class Main {
         //,UnaryOperator, BiPredicate, BiConsumer, BiFunction
 
         List<Integer> list=List.of(1,2,3,4,5,6,7,8,9,10);
-        list.stream().forEach(System.out::println);
+        list.stream().filter(num->num%2==0).forEach(System.out::println);
     }
 }
