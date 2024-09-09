f71e5ebe3b8964d8a69568bf6636506fcbc5cee2
sscstack <shashanktech2@gmail.com>
2024-09-09 11:11:14 +0530

    Multithreading in Java - Callable Interface

--- java-concepts/src/main/java/org/example/concurrency/v1/Main.java
+++ java-concepts/src/main/java/org/example/concurrency/v1/Main.java
@@ -1,5 +1,11 @@
 package org.example.concurrency.v1;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
 
     public static void numberPrinterTest() throws InterruptedException {
@@ -21,9 +27,31 @@ public class Main {
         thread2.join();
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void executorServiceTest() throws ExecutionException, InterruptedException {
+        ExecutorService executorService= Executors.newFixedThreadPool(2);
+        NumberPrinterV1 numberPrinter1 = new NumberPrinterV1(1, 50);
+        NumberPrinterV1 numberPrinter2 = new NumberPrinterV1(51, 100);
+        Future future1 = executorService.submit(numberPrinter1);
+        Future future2 = executorService.submit(numberPrinter2);
+        System.out.println(future1.isDone()+" "+future2.isDone());
+        executorService.shutdown();
+    }
+
+    public static void executorServiceCallableTaskTest() throws ExecutionException, InterruptedException {
+        ExecutorService executorService= Executors.newCachedThreadPool();
+        Future<String> welcomeMessage=executorService.submit(()->"Hello Shashank");
+        Future<String> welcomeMessage2=executorService.submit(()->"Hello Conrad");
+        System.out.println(welcomeMessage.get());
+        System.out.println(welcomeMessage2.get());
+        System.out.println("Main Exit");
+        executorService.shutdown();
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         //numberPrinterTest();
-        numberPrinterV1Test();
+        //numberPrinterV1Test();
+        //executorServiceTest();
+        executorServiceCallableTaskTest();
         System.out.println(Thread.currentThread().getName() + " started!");
     }
 }
