46a3863ab90e4ec74fb1ce8020a3866ee6d28e58
sscstack <shashanktech2@gmail.com>
2024-09-09 08:26:17 +0530

    Multithreading in Java

--- java-concepts/src/main/java/org/example/concurrency/v1/NumberPrinterV1.java
+++ java-concepts/src/main/java/org/example/concurrency/v1/NumberPrinterV1.java
@@ -1,4 +1,21 @@
 package org.example.concurrency.v1;
 
-public class NumberPrinterV1 {
+public class NumberPrinterV1 implements Runnable{
+    private int start;
+    private int end;
+
+    public NumberPrinterV1(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + " started!");
+        for (int i = start; i <= end; ++i) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n"+threadName + " ended.");
+    }
 }
