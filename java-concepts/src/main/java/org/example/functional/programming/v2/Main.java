9aadbd79fde34cb43412a21e386304b139b45611
sscstack <shashanktech2@gmail.com>
2024-10-15 10:42:32 +0530

    Printing Numbers using method reference

--- java-concepts/src/main/java/org/example/functional/programming/v2/Main.java
+++ java-concepts/src/main/java/org/example/functional/programming/v2/Main.java
@@ -1,7 +1,17 @@
 package org.example.functional.programming.v2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+
+    public static void printNumbers(int number) {
+        System.out.print(number+" ");
+    }
+    public static void main(String[] args) {
+        List<Integer> numbers=List.of(1,2,3,4,5,6,7,8,9,10);
+        //numbers.stream().forEach(Main::printNumbers);
+        //numbers.stream().forEach(number-> System.out.print(number+" "));
+        numbers.stream().forEach(System.out::println);
     }
 }
