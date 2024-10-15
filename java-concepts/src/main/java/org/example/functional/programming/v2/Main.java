892feba8a9a6542f76b5ea14be35dd2f583fbcf4
sscstack <shashanktech2@gmail.com>
2024-10-15 10:46:47 +0530

    Printing Even Numbers

--- java-concepts/src/main/java/org/example/functional/programming/v2/Main.java
+++ java-concepts/src/main/java/org/example/functional/programming/v2/Main.java
@@ -8,10 +8,16 @@ public class Main {
     public static void printNumbers(int number) {
         System.out.print(number+" ");
     }
+
+    public static void printEvenNumbers(List<Integer> numbers) {
+        numbers.stream().filter(number->number%2==0).forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
         List<Integer> numbers=List.of(1,2,3,4,5,6,7,8,9,10);
         //numbers.stream().forEach(Main::printNumbers);
         //numbers.stream().forEach(number-> System.out.print(number+" "));
-        numbers.stream().forEach(System.out::println);
+        //numbers.stream().forEach(System.out::println);
+        //printEvenNumbers(numbers);
     }
 }
